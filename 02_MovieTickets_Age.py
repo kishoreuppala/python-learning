e9d1d9a04028b39a9e104acc477366fc5cf753c3
Uppala Kishore <nagakishore64@gmail.com>
2024-08-30 11:14:41 +0530

    changes: Ticket prices based on age optimised

--- 02_MovieTickets_Age.py
+++ 02_MovieTickets_Age.py
@@ -1,19 +1,27 @@
 age = int(input ("Enter Your age: "))
 day = "Wednesday"
-children_price = 8
-adult_price = 12
 
-if day == "Wednesday":
-    children_price-=2
-    adult_price-=2
-    if age < 18:
-        print(f"Ticket price for you is: ${children_price}")
-    elif age >= 18:
-        print(f"Ticket price for you is: ${adult_price}")
-else:
-    if age < 18:
-        print(f"Ticket price for you is: ${children_price}")
-    elif age >= 18:
-        print(f"Ticket price for you is: ${adult_price}")
+price = 12 if age >= 18 else 8
+
+if day ==  "Wednesday":
+    price-=2
+
+print(f"Ticket price for you is ${price}")
+
+# children_price = 8
+# adult_price = 12
+
+# if day == "Wednesday":
+#     children_price-=2
+#     adult_price-=2
+#     if age < 18:
+#         print(f"Ticket price for you is: ${children_price}")
+#     elif age >= 18:
+#         print(f"Ticket price for you is: ${adult_price}")
+# else:
+#     if age < 18:
+#         print(f"Ticket price for you is: ${children_price}")
+#     elif age >= 18:
+#         print(f"Ticket price for you is: ${adult_price}")
     
               
