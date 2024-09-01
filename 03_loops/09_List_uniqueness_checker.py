57fd07ec5981fd0f6e87bb1c5c00cc982e7f3e25
Uppala Kishore <nagakishore64@gmail.com>
2024-09-01 18:52:49 +0530

    changes: list uniqueness checker

---
+++ 03_loops/09_List_uniqueness_checker.py
@@ -0,0 +1,12 @@
+# Check if all elements in a list are unique. If a duplicate is found, exit the loop and print the duplicate.
+items = ["apple", "banana", "orange", "apple", "mango"]
+unique_item=set()
+
+for item in items:
+    if item in unique_item:
+        print(f"Duplicate item: {item}")
+        # print(unique_item)
+        break
+    else:
+        unique_item.add(item)
+
