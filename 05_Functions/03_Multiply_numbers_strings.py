efdd4aef2eec8e6aac0116b61e69f32244621ebd
Uppala Kishore <nagakishore64@gmail.com>
2024-09-22 14:13:52 +0530

    changes: function that multiply numbers and strings

---
+++ 05_Functions/03_Multiply_numbers_strings.py
@@ -0,0 +1,7 @@
+#  Write a function multiply that multiplies two numbers, but can also accept and multiply strings
+
+def multiply(a,b):
+    return a * b
+
+# print(multiply(5,5))
+print(multiply("Kishore",3))
