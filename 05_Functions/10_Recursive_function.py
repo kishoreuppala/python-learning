15c58de54ab5ffeecb9845aad5d6859713434986
Uppala Kishore <nagakishore64@gmail.com>
2024-09-25 11:49:19 +0530

    recursive function to calculate the factorial of a number.

---
+++ 05_Functions/10_Recursive_function.py
@@ -0,0 +1,9 @@
+# Create a recursive function to calculate the factorial of a number.
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+print(factorial(5)) 
+print(factorial(10)) 
